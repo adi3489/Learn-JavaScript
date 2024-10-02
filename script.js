49593425c0cfffbf66a1dff346584c972b6176f7
adi3489 <07aditya.cn@gmail.com>
2024-10-02 23:05:46 +0530

    JS

---
+++ script.js
@@ -0,0 +1,70 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JS is fun");
+// console.log(40 + 8 + 23 - 10);
+// let firstName = "Jonas";
+// console.log(firstName);
+/*
+let javaScriptFun = true;
+console.log(javaScriptFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+
+javaScriptFun = "Yes";
+console.log(typeof javaScriptFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+
+// var job = "programmer";
+// job = "teacher";
+
+// lastName = "Aditya";
+// console.log(lastName);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// const firstName = "Aditya";
+// const lastName = "Chauhan";
+// console.log(firstName + " " + lastName);
+
+// //assingment operator
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// console.log(x);
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = "1991";
+// const year = 2037;
+
+// const jonas =
+//   "I'm" + " " + firstName + ",a" + (year - birthYear) + "years old" + job + "!";
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName},a ${year - birthYear} year old ${job}`;
+// console.log(jonasNew);
+
+// console.log(`just a regular string baby...`);
+
+// console.log(`string
+//     multiple
+//     lines`);
