215d13c73ffc50bf7d9161be53d32785ee65a913
adi3489 <07aditya.cn@gmail.com>
2024-10-03 19:47:23 +0530

    js fundamental 2

---
+++ js fundamental 2/script.js
@@ -0,0 +1,22 @@
+"use strict";
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive :D");
+
+function logger() {
+  console.log("My name is Aditya");
+}
+
+logger(); //calling,runnung,invoking the funtion
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
