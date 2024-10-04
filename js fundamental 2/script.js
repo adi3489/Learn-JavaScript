d2d08c675e5c9fefb0ecbfa2fe709070d2ed0290
adi3489 <07aditya.cn@gmail.com>
2024-10-05 00:13:50 +0530

    arrow function

--- js fundamental 2/script.js
+++ js fundamental 2/script.js
@@ -5,18 +5,53 @@
 // if (passTest) hasDriversLicense = true;
 // if (hasDriversLicense) console.log("I can drive :D");
 
-function logger() {
-  console.log("My name is Aditya");
-}
+// function logger() {
+//   console.log("My name is Aditya");
+// }
 
-logger(); //calling,runnung,invoking the funtion
-logger();
-logger();
+// logger(); //calling,runnung,invoking the funtion
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-  console.log(apples, oranges);
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// function fruitProcessor(apples, oranges) {
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// function calcAge1(birthYear) {
+//   //function declaration
+//   //   const age = 2037 - birthYear;
+//   return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991); //function expression
+
+// const calcAge2 = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
+// const age2 = calcAge2(1991);
+
+//Arrow function
+
+// const calcAge3 = (birthYeah) => 2037 - birthYeah;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  //   return retirement;
+  return `${firstName} retires in ${retirement}`;
+};
+
+console.log(yearUntilRetirement(1991, "Aditya"));
+// const yearUntilRetirement = (birthYear) => {
+//   const age = 2037 - birthYear; // Ensure the same variable is used here
+//   const retirement = 65 - age;
+//   return retirement;
+// };
+
+// console.log(yearUntilRetirement(1991));
