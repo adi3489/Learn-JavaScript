7e68233d137038b506b101c9008bc92f3b836ff0
adi3489 <07aditya.cn@gmail.com>
2024-10-15 10:35:03 +0530

    js

--- js fundamental 2/script.js
+++ js fundamental 2/script.js
@@ -135,3 +135,17 @@
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Aditya ${rep}`);
 }
+const jonas = [
+  "jonas",
+  "Schmedtmann",
+  "2037-1991",
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+  types[i] = typeof jonas[i];
+}
+console.log(types);
